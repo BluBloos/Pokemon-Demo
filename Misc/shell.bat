5dfb6c48422556c36baebdbc0f62b990a79011a6
BluBloos <cnoah1705@gmail.com>
2021-01-07 15:49:41 -0500

    Got rid of legacy Misc folder

--- Misc/shell.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
-set path=C:\PokemonDemo\Misc;C:\PokemonDemo\Code;C:\PokemonDemo\build;%path%
-cd C:\PokemonDemo\build
