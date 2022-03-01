0ee5272b219349f72cf940ce4c0a83c3f443dba1
BluBloos <cnoah1705@gmail.com>
2022-03-01 07:34:49 -0500

    Windows build working

--- build.bat
+++ build.bat
@@ -1,21 +1,27 @@
 @echo off
 
-set CommonCompilerFlags=-nologo -Gm- -EHa- -GR- -Oi -W4 -wd4293 -wd4456 -wd4100 -wd4189 -wd4505 -wd4701 -DPOKEMON_DEMO_DEBUG=1 -FC -Zi -RTCs -I ..\Code
+set CommonCompilerFlags=-nologo -Gm- -EHa- -GR- -Oi -W4 -wd4293 -wd4456 -wd4100 -wd4189 -wd4505 -wd4701 ^
+    -DPOKEMON_DEMO_DEBUG=1 -FC -Zi -RTCs -I ..\Code
 
 set CommonLinkerFlags=-incremental:no -opt:ref user32.lib gdi32.lib winmm.lib dinput8.lib dxguid.lib
 
-pushd build
+mkdir build
+mkdir build\windows
+
+pushd build\windows
 
 del *.pdb > NUL 2> NUL
 
-cl %CommonCompilerFlags% -Fmwin32_PokemonDemo.map ..\Code\win32_PokemonDemo.cpp /link -subsystem:windows,5.1 %CommonLinkerFlags%
+cl %CommonCompilerFlags% -Fmwin32_PokemonDemo.map ..\..\src\win32_PokemonDemo.cpp /link -subsystem:windows,5.1 %CommonLinkerFlags%
 
-cl %CommonCompilerFlags% -FmPokemonDemo.map ..\Code\PokemonDemo.cpp -LD /link -subsystem:windows,5.1 -PDB:PokemonDemo_%random%.pdb -incremental:no -EXPORT:GameGetSoundSamples -EXPORT:GameUpdateRender
+cl %CommonCompilerFlags% -FmPokemonDemo.map ..\..\src\PokemonDemo.cpp -LD /link -subsystem:windows,5.1 -PDB:PokemonDemo_%random%.pdb ^
+    -incremental:no -EXPORT:GameGetSoundSamples -EXPORT:GameUpdateRender
 
-cl %CommonCompilerFlags% -FmPokemonDemoTileEditor.map ..\Code\win32_PokemonDemo_tile_map_editor.cpp /link -subsystem:windows,5.1 %CommonLinkerFlags%
+cl %CommonCompilerFlags% -FmPokemonDemoTileEditor.map ..\..\src\win32_PokemonDemo_tile_map_editor.cpp /link ^
+    -subsystem:windows,5.1 %CommonLinkerFlags%
 
-cl %CommonCompilerFlags% ..\Code\win32_PikaBlue_Cmd.cpp /link -subsystem:console,5.1 %CommonLinkerFlags%
+cl %CommonCompilerFlags% ..\..\src\win32_PikaBlue_Cmd.cpp /link -subsystem:console,5.1 %CommonLinkerFlags%
 
-cl %CommonCompilerFlags% -DPIKABLUE_UI=1 ..\Code\win32_PikaBlue_main.cpp /link -subsystem:windows,5.1 %CommonLinkerFlags% Comdlg32.lib
+cl %CommonCompilerFlags% -DPIKABLUE_UI=1 ..\..\src\win32_PikaBlue_main.cpp /link -subsystem:windows,5.1 %CommonLinkerFlags% Comdlg32.lib
 
 popd
